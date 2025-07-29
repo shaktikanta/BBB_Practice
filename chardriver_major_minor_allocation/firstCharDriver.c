da69653ff2737474f0668e99bd6907ef197ec382
Shakti <nayak.shaktikanta@gmail.com>
2025-07-29 16:49:23 +0000

    chardriver: major and minor no allocation
    - sample driver for allocating major and minor usage.
    
    Signed-off-by: Shakti <nayak.shaktikanta@gmail.com>

---
+++ chardriver_major_minor_allocation/firstCharDriver.c
@@ -0,0 +1,36 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/kdev_t.h>
+
+
+/* Allocate statically the Major and Minor No */
+dev_t dev = MKDEV(235, 0);
+
+
+/* Init Method */
+static int __init firstCharDriver_init(void) {
+    if(register_chrdev_region(dev, 1, "First Char Driver") == 0){
+        printk(KERN_INFO "MAJOR: %d - MINOR: %d\n", MAJOR(dev), MINOR(dev));
+        printk("Module Loaded Sucessfully.\n");
+    } else {
+        printk(KERN_INFO "Allocating driver no is failed.\n");
+    }
+    return 0;
+}
+
+/* Exit Method */
+void __exit firstCharDriver_exit(void) {
+    printk(KERN_INFO "Module removed successfully.\n");
+}
+
+
+/* Invoking init and exit function */
+module_init(firstCharDriver_init);
+module_exit(firstCharDriver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Shakti");
+MODULE_DESCRIPTION("Allocating Major and Minor no example for a char driver");
+MODULE_VERSION("1.0.3");
---
+++ chardriver_major_minor_allocation/firstCharDriver.mod.c
@@ -0,0 +1,37 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0x3726c6aa, "module_layout" },
+	{ 0xefd6cf06, "__aeabi_unwind_cpp_pr0" },
+	{ 0xc5850110, "printk" },
+	{ 0x3fd78f3b, "register_chrdev_region" },
+};
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "37AABC51B41D982E9B2F7D8");
